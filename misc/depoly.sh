f1c55ecb559949eb92b7af72836337f8154971aa
Kevinzjy <kevinzjy1997@gmail.com>
2021-04-21 19:53:55 +0800

    add Dockerfile for  centos7

--- misc/depoly.sh
+++ misc/depoly.sh
@@ -1,31 +1,21 @@
 #!/bin/bash
-# Install on CentOS 7 with glibc 2.17
-
-# Install dependencies
-strings /lib64/libc.so.6 | grep -P "^GLIBC"
-yum groupinstall "Development Tools"
-yum install git openssl-devel
-
-# Update cmake
-wget https://github.com/Kitware/CMake/releases/download/v3.20.1/cmake-3.20.1.tar.gz
-tar zxvf cmake-3.20.1.tar.gz
-cd cmake-3.20.1
-configure && gmake && make install 
-
-wget http://zlib.net/zlib-1.2.11.tar.gz
-tar zxvf zlib-1.1.11.tar.gz
-cd zlib-1.2.11
-configure && make && make install
-
-# Install Rust
-useradd rust
-su - rust
-curl https://sh.rustup.rs -sSf | sh
-
-# Compile circtools
 git clone --recursive https://github.com/Kevinzjy/circtools.git
-cd circtools
-cargo build --release
+cd circtools/misc
+
+podman build -t centos7_rust:v1 ./
+
+setproxy
+
+podman run -v \
+  /home/zhangjy/data/git/circtools:/volume:z \
+  -w /volume \
+  --userns keep-id \
+  --network host \
+  --rm \
+  -t localhost/centos7_rust:v1 cargo test --verbose && cargo build --verbose --release
+
+strip ./target/release/ccs
+
 mv ./target/release/ccs ./target/release/ccs_v1.0.0_el7.x86-64
 
 # RUSTFLAGS=-g cargo build --release
